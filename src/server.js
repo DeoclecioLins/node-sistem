9fcee0aa38a5bc1f9d3c9293fa683b5e92b2ae55
deoclecio.lins <deoclecio.lins@cgm.fortaleza.ce.gov.br>
2020-09-22 00:50:18 -0300

    ultimos passos

--- src/server.js
+++ src/server.js
@@ -3,10 +3,11 @@ const bodyParser = require('body-parser');
 const routes = require('./routes');
 require('./database');
 
+
 const app = express();
 app.use(express.json());
 app.use(routes);
 app.use(bodyParser.urlencoded({ extended: false }));
-require('./app/controllers/authController')(app);
+
 
 app.listen(3333);
